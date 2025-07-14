2ddd1a9534e254863fee8115e7cc54dddfab356b
tcaminel-pro <thierry.caminel@eviden.com>
2025-07-14 21:17:31 +0200

    refactor: update welcome page comment for direction indicator

--- src/webapp/pages/0_welcome.py
+++ src/webapp/pages/0_welcome.py
@@ -22,6 +22,7 @@ title_col1.title("Welcome ! ")
 LOGO = "New Atos logo white.png"
 logo = str(Path.cwd() / "src/webapp/static" / LOGO)
 title_col2.image(logo, width=250)
+# replace ☜ by something showing up direction AI!
 title_col1.markdown(
     """
     ### ☜ ☜ Select a demo or a Playground! 
